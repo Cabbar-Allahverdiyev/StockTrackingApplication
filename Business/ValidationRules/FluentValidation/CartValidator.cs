a9f7c64a4948938a294ec6bb6fb613001316b433
cabbar.allahverdiyev@gmail.com <cabbar.allahverdiyev@gmail.com>
2022-01-18 01:53:35 +0400

    CartValidator renewed

--- Business/ValidationRules/FluentValidation/CartValidator.cs
+++ Business/ValidationRules/FluentValidation/CartValidator.cs
@@ -1,4 +1,5 @@
-﻿using Core.CrossCuttingConcerns.Validation.Abstract;
+﻿using Business.Constants.Messages;
+using Core.CrossCuttingConcerns.Validation.Abstract;
 using Entities.Concrete;
 using FluentValidation;
 using System;
@@ -11,12 +12,32 @@ namespace Business.ValidationRules.FluentValidation
     {
         public CartValidator()
         {
-            RuleFor(c=>c.ProductId).NotEmpty();
-            RuleFor(c=>c.Quantity).NotEmpty();
-            RuleFor(c=>c.SoldPrice).NotEmpty();
-            RuleFor(c=>c.TotalPrice).NotEmpty();
-         
-          
+            RuleFor(c => c.ProductId).NotEmpty();
+            RuleFor(c => c.ProductId).GreaterThan(0);
+
+            RuleFor(c => c.Quantity).NotEmpty();
+            RuleFor(c => c.Quantity).GreaterThan(0);
+            RuleFor(c => c.Quantity).Must(MustNotBeLessThanZero).WithMessage(CartMessages.QuantityMustBeGreaterThanZero);
+
+            RuleFor(c => c.SoldPrice).NotEmpty();
+            RuleFor(c => c.SoldPrice).GreaterThan(0);
+            RuleFor(c => c.SoldPrice).Must(MustNotBeLessThanZero).WithMessage(CartMessages.QuantityMustBeGreaterThanZero);
+
+            RuleFor(c => c.TotalPrice).NotEmpty();
+            RuleFor(c => c.TotalPrice).GreaterThan(0);
+            RuleFor(c => c.TotalPrice).Must(MustNotBeLessThanZero).WithMessage(CartMessages.QuantityMustBeGreaterThanZero);
+
+
+        }
+
+        private bool MustNotBeLessThanZero(int value)
+        {
+            return value > 0;
+        }
+
+        private bool MustNotBeLessThanZero(decimal value)
+        {
+            return value > 0;
         }
     }
 }
