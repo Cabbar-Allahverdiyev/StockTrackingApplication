ba67972c6d692b07821f81a2188e4c807ad23854
cabbar allahverdiyev <Cabbar.Allahverdiyev@gmail.com>
2022-02-02 00:28:00 +0400

    TextBoxController add

---
+++ WindowsForm/Core/Controllers/Concrete/TextBoxController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsForm.Core.Controllers.Concrete
+{
+    public class TextBoxController
+    {
+        public static void ClearAllTextBoxesByGroupBox(GroupBox groupBox)
+        {
+            foreach (Control control in groupBox.Controls)
+            {
+                ClearAllTextBoxes(control);
+
+            }
+        }
+
+        public static void ClearAllTextBoxesAndCmboBoxesByGroupBox(GroupBox groupBox)
+        {
+            foreach (Control control in groupBox.Controls)
+            {
+                ClearAllTextBoxes(control);
+                ClearAllComboBoxes(control);
+            }
+        }
+
+
+        public static void ClearAllTextBoxes(Control control)
+        {
+            if (control is TextBox)
+            {
+                control.Text = "";
+            }
+        }
+
+        public static void ClearAllComboBoxes(Control control)
+        {
+            if (control is ComboBox)
+            {
+                control.Text = "";
+            }
+        }
+    }
+}
