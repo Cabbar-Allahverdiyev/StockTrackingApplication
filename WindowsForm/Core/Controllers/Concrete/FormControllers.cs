5042ccaf70d3998d9f7a2fd66cbb2469fb86b010
cabbar allahverdiyev <Cabbar.Allahverdiyev@gmail.com>
2022-02-05 15:43:37 +0400

    Form controller add

---
+++ WindowsForm/Core/Controllers/Concrete/FormControllers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsForm.Core.Controllers.Concrete
+{
+    public class FormControllers
+    {
+        public void AdjustForm(Form form)
+        {
+            int borderSize = 2;
+            switch (form.WindowState)
+            {
+                case FormWindowState.Maximized:
+                    form.Padding = new Padding(0, 8, 8, 0);
+                    break;
+                case FormWindowState.Normal:
+                    if (form.Top != borderSize)
+                        form.Padding = new Padding(borderSize);
+
+                    break;
+            }
+        }
+
+
+    }
+}
