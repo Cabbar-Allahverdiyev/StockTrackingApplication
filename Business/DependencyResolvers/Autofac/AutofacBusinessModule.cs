d80869e0d779a155156e350b84eb5fea754d768c
cabbar allahverdiyev <Cabbar.Allahverdiyev@gmail.com>
2022-03-09 22:19:22 +0400

    AutoFacBusinessModule updated

--- Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
+++ Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -20,6 +20,42 @@ namespace Business.DependencyResolvers.Autofac
             builder.RegisterType<UserManager>().As<IUserService>().SingleInstance();
             builder.RegisterType<EfUserDal>().As<IUserDal>().SingleInstance();
 
+            builder.RegisterType<BarcodeNumberManager>().As<IBarcodNumberService>().SingleInstance();
+            builder.RegisterType<EfBarcodeNumberDal>().As<IBarcodeNumberDal>().SingleInstance();
+
+            builder.RegisterType<BonusCardManager>().As<IBonusCardService>().SingleInstance();
+            builder.RegisterType<EfBonusCardDal>().As<IBonusCardDal>().SingleInstance();
+
+            builder.RegisterType<BrandManager>().As<IBrandService>().SingleInstance();
+            builder.RegisterType<EfBrandDal>().As<IBrandDal>().SingleInstance();
+
+            builder.RegisterType<CartManager>().As<ICartService>().SingleInstance();
+            builder.RegisterType<EfCartDal>().As<ICartDal>().SingleInstance();
+
+            builder.RegisterType<CategoryManager>().As<ICategoryService>().SingleInstance();
+            builder.RegisterType<EfCategoryDal>().As<ICategoryDal>().SingleInstance();
+
+            builder.RegisterType<CustomerManager>().As<ICustomerService>().SingleInstance();
+            builder.RegisterType<EfCustomerDal>().As<ICustomerDal>().SingleInstance();
+
+            builder.RegisterType<CustomerBalanceManager>().As<ICustomerBalanceService>().SingleInstance();
+            builder.RegisterType<EfCustomerBalanceDal>().As<ICustomerBalanceDal>().SingleInstance();
+
+            builder.RegisterType<CustomerPaymentManager>().As<ICustomerPaymentService>().SingleInstance();
+            builder.RegisterType<EfCustomerPaymentDal>().As<ICustomerPaymentDal>().SingleInstance();
+
+            builder.RegisterType<DebtManager>().As<IDebtService>().SingleInstance();
+            builder.RegisterType<EfDebtDal>().As<IDebtDal>().SingleInstance();
+
+            builder.RegisterType<ProductManager>().As<IProductService>().SingleInstance();
+            builder.RegisterType<EfProductDal>().As<IProductDal>().SingleInstance();
+
+            builder.RegisterType<SaleManager>().As<ISaleService>().SingleInstance();
+            builder.RegisterType<EfSaleDal>().As<ISaleDal>().SingleInstance();
+
+            builder.RegisterType<SupplierManager>().As<ISupplierServices>().SingleInstance();
+            builder.RegisterType<EfSupplierDal>().As<ISupplierDal>().SingleInstance();
+
             builder.RegisterType<AuthManager>().As<IAuthService>();
 
             builder.RegisterType<JwtHelper>().As<ITokenHelper>();
