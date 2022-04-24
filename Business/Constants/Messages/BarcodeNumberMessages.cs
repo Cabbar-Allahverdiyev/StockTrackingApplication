0e043614c28daffae5767ab198f0987657f169ea
cabbar allahverdiyev <Cabbar.Allahverdiyev@gmail.com>
2022-04-25 00:54:27 +0400

    Barcode Message updated

--- Business/Constants/Messages/BarcodeNumberMessages.cs
+++ Business/Constants/Messages/BarcodeNumberMessages.cs
@@ -17,12 +17,17 @@ namespace Business.Constants.Messages
         public static string BarcodNumberNotFound = "Belə bir Barkod nömrəsi yoxdur";
 
         public static string BarcodeNumberGenerated = "Verilən dəyərə uyğun barkod yaradıldı";
+        public static string BarcodeNumberLengthLessThan13NotGenerated = "Verilən dəyər '13' dən kiçik olduğu üçün barkod yaradıla bilmədi";
         public static string QRCodeNotGenerated = "Verilən dəyərə uyğun barkod yaradıla bilmədi!";
         public static string Scanned = "Scan edildi";
         public static string ScanFailed = "Scan edile bilmədi!";
         public static string QRCodeGenerated = "Verilən dəyərə uyğun QR kod yaradıldı";
         public static string SaveFailed = "Yaddaşa yazıla bilmədi";
+        public static string Save = "Yaddaşa yazıldı";
         public static string LoadFailed = "Fayl açıla bilmədi";
+        public static string Load = "Fayl açıldı";
         public static string RandomBarcodeNumberGenerated = "Təsadüfi barkod nömrəsi yaradıldı";
+        public static string BarcodeNumberNotSelected = "Barkod nömrəsi seçilə bilmədi";
+        public static string BarcodeNumberSelected = "Barkod nömrəsi seçildi";
     }
 }
