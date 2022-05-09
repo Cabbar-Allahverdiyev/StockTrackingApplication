41b7ab56f7e8897e6f7036a8aa9f830e8a355054
cabbar allahverdiyev <Cabbar.Allahverdiyev@gmail.com>
2022-05-10 00:15:55 +0400

    Customer Search system added

---
+++ WindowsForm/Utilities/Search/Concrete/CustomerSearch/CustomerSearch.cs
@@ -0,0 +1,24 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using WindowsForm.Utilities.Search.Abstract;
+
+namespace WindowsForm.Utilities.Search.Concrete.CustomerSearch
+{
+    public class CustomerSearch : IWinFormsSearch<Customer>
+    {
+        public void GetDataWriteGridView(List<Customer> data, string seachText, DataGridView dataGridView)
+        {
+            dataGridView.DataSource = Search(data,seachText);
+        }
+
+        public List<Customer> Search(List<Customer> data, string searchText)
+        {
+            return data.Where(c => $"{c.Id} {c.FirstName} {c.LastName} {c.PhoneNumber} {c.Address} {c.Email} {c.CreatedDate}"
+           .ToLower().Contains(searchText.ToLower())).ToList();
+        }
+    }
+}
