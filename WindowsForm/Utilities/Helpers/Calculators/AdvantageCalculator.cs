c5c439ac41975f2c7c376f03f11c4988b16ab23e
cabbar allahverdiyev <Cabbar.Allahverdiyev@gmail.com>
2022-06-09 23:41:19 +0400

    InterestedAdvantageCalculator

---
+++ WindowsForm/Utilities/Helpers/Calculators/AdvantageCalculator.cs
@@ -0,0 +1,31 @@
+﻿using Business.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WindowsForm.Core.Constants.ControllerNames.SettingForm;
+using WindowsForm.Core.Constants.Messages.WeekMessgaes;
+
+namespace WindowsForm.Utilities.Helpers.Calculators
+{
+    public static class AdvantageCalculator
+    {
+        public static int SelectedDayWhichOfTheWeek(string day)
+        {
+            List<string> days = WeekMessages.AzerbaijaniMessages();
+            for (int i = 0; i < days.Count; i++)
+            {
+                if (days[i] == day)
+                    return i + 1;
+            }
+            return -1;
+        }
+
+        public static decimal WhatIsAdvantageToday(IFormSettingService _formSettingService)
+        {
+            int day = SelectedDayWhichOfTheWeek(_formSettingService.GetByName(SettingControllerName.ComboBoxWhichDays).Data.Value);
+            if (day == (int)DateTime.Now.DayOfWeek)
+                return decimal.Parse(_formSettingService.GetByName(SettingControllerName.TextBoxWeeklyInterestRate).Data.Value);
+            return decimal.Parse(_formSettingService.GetByName(SettingControllerName.TextBoxIGeneralInterestRate).Data.Value);
+        }
+    }
+}
