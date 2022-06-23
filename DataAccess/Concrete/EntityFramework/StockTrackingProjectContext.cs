8bf8ffc393b8ceff88c9794f50830f72b9507133
Cabbar Allahverdiyev <cabbar.allaherdiyev@gmail.com>
2022-06-23 16:41:08 +0400

    ..

--- DataAccess/Concrete/EntityFramework/StockTrackingProjectContext.cs
+++ DataAccess/Concrete/EntityFramework/StockTrackingProjectContext.cs
@@ -13,8 +13,8 @@ namespace DataAccess.Concrete.EntityFramework
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             //optionsBuilder.UseSqlServer(@"Server=DESKTOP-N8AGODS\MSSQLSERVER66;Database=StockTracking;Trusted_Connection=true");
-           // optionsBuilder.UseSqlServer(@"Server=(localdb)\MsSqlLocalDb;Database=StockTracking;Trusted_Connection=true"); //menim T-SQL
-            optionsBuilder.UseNpgsql(@"Server=localhost;Database=TapSat;Port=5432;User Id=postgres;Password=Cabiw****"); //menim Postgre
+           optionsBuilder.UseSqlServer(@"Server=(localdb)\MsSqlLocalDb;Database=StockTracking;Trusted_Connection=true"); //menim T-SQL
+           // optionsBuilder.UseNpgsql(@"Server=localhost;Database=TapSat;Port=5432;User Id=postgres;Password=Cabiw****"); //menim Postgre
            //optionsBuilder.UseSqlServer(@"Server=DESKTOP-SN5E5RA\SQLEXPRESS01;Database=StockTracking;Trusted_Connection=true"); //Murad IphoneShop
         }
 
