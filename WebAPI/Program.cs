6158f19885d773e428f23e4bc0726592c5d5611e
Cabbar Allahverdiyev <cabbar.allaherdiyev@gmail.com>
2022-07-14 16:53:10 +0400

    .

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -21,7 +21,7 @@ namespace WebAPI
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
+            Host.CreateDefaultBuilder(args) 
                 .UseServiceProviderFactory(new AutofacServiceProviderFactory())
                 .ConfigureContainer<ContainerBuilder>(builder =>
                 {
