bc0966ff370a24267ec52fd07e7428bd41bc7917
Cabbar Allahverdiyev <cabbar.allahverdiyev@gmail.com>
2022-07-23 00:35:26 +0400

    /

--- Entities/Concrete/Supplier.cs
+++ Entities/Concrete/Supplier.cs
@@ -10,7 +10,7 @@ namespace Entities.Concrete
         public int Id { get; set; }
         public string CompanyName { get; set; }
         public string ContactName { get; set; }
-        public string WhenWillCome { get; set; }
+        public string WhenWillCome { get; set; } 
         public string Address { get; set; }
         public string City { get; set; }
         public string Email { get; set; }
