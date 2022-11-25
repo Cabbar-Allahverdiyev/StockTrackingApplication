2ae54f292b57497a8a640956a65eec4225ab08b4
Cabbar Allahverdiyev <cabbar.allahverdiyev@gmail.com>
2022-11-25 22:11:24 +0400

    new dictionary added

---
+++ Business/Constants/Dictionaries/SettingsDictionary.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static Business.Constants.Enums.SettingEnums;
+
+namespace Business.Constants.Dictionaries
+{
+    public static class SettingsDictionary
+    {
+        public static readonly IDictionary<SettingParameter, string>
+     Settings = new Dictionary<SettingParameter, string>
+     {
+            { SettingParameter.WhichDays, "comboBoxWhichDays" },
+            { SettingParameter.GeneralInterestRate, "textBoxIGeneralInterestRate" },
+            { SettingParameter.WeeklyInterestRate, "textBoxWeeklyInterestRate" },
+            { SettingParameter.SignsDecimalPoint, "textBoxSignsDecimalPoint" },
+            { SettingParameter.BonusCardBarcodeLenth, "textBoxBonusCardBarcodeLenth" },
+            { SettingParameter.BonusType, "textBoxBonusType" },
+            { SettingParameter.ShopName, "textBoxShopName" },
+            { SettingParameter.ShopAddress, "textBoxShopAddress" },
+            { SettingParameter.ShopNumber, "textBoxShopNumber" },
+            { SettingParameter.Voen, "textBoxVoen" },
+            { SettingParameter.VaultModel, "textBoxVaultModel" },
+            { SettingParameter.VaultSerialNumber, "textBoxVaultSerialNumber" },
+            { SettingParameter.ReceiptNumber, "ReceiptNumber" },
+            { SettingParameter.ReceiptGivenOnTheDay, "ReceiptGivenOnTheDay" },
+
+     };
+    }
+}
