5b1d0b2a670747803e5ee82a6000b64ecb55f037
Cabbar Allahverdiyev <cabbar.allahverdiyev@gmail.com>
2022-11-25 22:11:37 +0400

    new enums added

---
+++ Business/Constants/Enums/SettingEnums.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Constants.Enums
+{
+    public static class SettingEnums
+    {
+        public enum SettingParameter
+        {
+            WhichDays,
+            GeneralInterestRate,
+            WeeklyInterestRate,
+            SignsDecimalPoint,
+            BonusCardBarcodeLenth,
+            BonusType,
+            ShopName,
+            ShopAddress,
+            ShopNumber,
+            Voen,
+            VaultModel,
+            VaultSerialNumber,
+            ReceiptNumber,
+            ReceiptGivenOnTheDay
+
+        }
+    }
+}
